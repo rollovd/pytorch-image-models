c9add697dee77347c49a498cfaff67ea1e6fd928
rollovd <rollov23@gmail.com>
2024-08-12 21:52:57 +0500

    explicitly coverage

--- timm/models/crossvit.py
+++ timm/models/crossvit.py
@@ -449,8 +449,6 @@ class CrossVit(nn.Module):
 
             if is_horizontal is not None:
                 orientation_embed = torch.ones(B, 1, x_.size(-1), device=x_.device)
-
-                print(orientation_embed.shape, is_horizontal.shape)
                 orientation_embed[~is_horizontal] = 0
                 x_ = torch.cat((x_, orientation_embed), dim=1)
 
