c78a63f2f484bc12f6f62fc7828fd18903081ea0
rollovd <rollov23@gmail.com>
2024-08-13 13:49:51 +0500

    trunc normal for pos embed for square

--- timm/models/crossvit.py
+++ timm/models/crossvit.py
@@ -463,7 +463,8 @@ class CrossVit(nn.Module):
             if self.num_squares_for_positional_embedding is not None:
                 branch_positional_embedding_for_squares = getattr(self, f'pos_embed_for_square_{i}')
                 print(branch_positional_embedding_for_squares.shape)
-                branch_positional_embedding_for_squares = branch_positional_embedding_for_squares.repeat(1, 1, 1)
+                branch_positional_embedding_for_squares = branch_positional_embedding_for_squares.repeat(
+                    B // self.num_squares_for_positional_embedding, 1, 1)
                 print(branch_positional_embedding_for_squares.shape)
                 print(x_.shape)
                 x_ = x_ + branch_positional_embedding_for_squares
